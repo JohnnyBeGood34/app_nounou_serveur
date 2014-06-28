174b2df034b58fa1514af111c40b4d1d8e4e941f
JohnnyBeGood34 <jonathanaffre@gmail.com>
2014-06-28 13:53:05 +0200

    distance = 0 quand on appelle la liste complete de toute les nounous

--- nounou/controllers/api.js
+++ nounou/controllers/api.js
@@ -39,6 +39,11 @@ module.exports = {
                 return res.send({"code":404,"status":404, "message":"not found"});
             }
             else{
+                //Si on appele le get all nounou c'est qu'il n'y a pas de GPS donc distance = 0
+                eachAsync(json,function(element,index,done){
+                    element.distance = 0;
+                    done();
+                });
                 res.send({allNounous:json});
             }
         }).limit(50);
